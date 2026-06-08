da391077b316a1860daa16dcaee82f66db03c4b5
ksdhanush-strack <ksdhanush1989@gmail.com>
2026-06-08 11:24:38 +0530

    Add mathematical operations and shape calculations
    This script performs various mathematical operations including addition, subtraction, multiplication, and division. It also calculates the area and perimeter of shapes, averages three numbers, checks number equality, and demonstrates variable manipulation.

---
+++ task 2.py
@@ -0,0 +1,117 @@
+#lab task 2
+
+1) a=int(input("enter first number :"))
+b=int(input("enter second number:"))
+print("addition =",a+b)
+print("subraction =",a-b)
+print("multipication=",a*b)
+print("division=",a/b)
+print("modulas =",a%b)
+print("floor division =",a//b)
+print("exponention=",a**b)
+
+
+2) #rectangle
+
+l= float (input("enter length:"))
+w=float(input("enter width:"))
+print("area =",l*w)
+print("perimeter =",2*(l+w)
+
+
+#square
+
+s=float(input("enter side:"))
+print("area =",s*s)
+print("perimeter = ",4*s)
+
+
+#circle
+
+import math
+r=float(input("enter radius:"))
+print("area =",math.pi*r*r)
+print("circumference =",2*math.pi
+
+      
+3) a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=int(input("enter third number:"))
+
+avg =(a +b+c)/3
+print("average =",avg)
+
+
+
+4) a= int(input("enter first number:"))
+b=int(input("enter second number:"))
+print("equal:",a==b)
+print("greater:",a<b)
+print("less than or equal:",a<=b)
+
+5) import math
+n=float (input("enter a n umber:"))
+
+6) p=float(input("prinicipal:"))
+r=float(input("rate:"))
+t=float(input("time:"))
+si=(p*r*t)/100
+ci=p*((1*r/100)**t)=p
+
+print("simple intrest =",si)
+print("compound intrest =",ci)
+
+7)
+x=10
+
+x+=5
+print(x)
+
+x-=3
+print(x)
+
+x*=2
+print(x)
+
+x/=4
+print(x)
+
+x %=2
+print(x)
+
+x**=3
+print(x)
+
+
+8) a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+
+a=a+b
+b=a-b
+a=a-b
+
+print("after swapping:")
+print("a =",a)
+print("b =",b)
+
+
+9) n=float(input("enter a number:"))
+cube_root =n**(1/3)
+print("cube root =", cube_root)
+
+
+10) num=8523
+print("last 2 digit =,num%100)
+      
+
+11) num=8523
+print("after removing last 2 digits =",num//100)
+
+
+
+
+
+      
+
+
+
